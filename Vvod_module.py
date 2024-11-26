39d449cb62c5fed1e9b2070bed1fe2c3f28051ee
Евгений Цитцер <evgenc1980@mail.ru>
2024-11-26 16:14:37 +0700

    Add files via upload

---
+++ Vvod_module.py
@@ -0,0 +1,25 @@
+"1st program"
+
+print(9**0.5*5)
+
+"2nd program"
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+"3rd program"
+
+a = 2*2+2
+b = 2*(2+2)
+print(a)
+print(b)
+print(a == b)
+
+"4th program"
+
+str_ = '123.456'
+fl_ = float(str_)
+fl_2 = fl_ * 10
+print(int(fl_2) % 10)
+
+
+
